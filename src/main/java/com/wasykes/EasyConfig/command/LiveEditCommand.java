fd98d7c70e3eee2a2da4b687d9f32abc138c2471
darknuju <darknuju@gmail.com>
2018-10-31 16:22:29 -0400

    Basic implementation of set command

--- src/main/java/com/wasykes/EasyConfig/command/LiveEditCommand.java
+++ src/main/java/com/wasykes/EasyConfig/command/LiveEditCommand.java
@@ -1,9 +1,14 @@
 package com.wasykes.EasyConfig.command;
 
+import com.google.common.collect.Sets;
 import com.wasykes.EasyConfig.ConfigComponent;
 import com.wasykes.EasyConfig.EasyConfig;
 import com.wasykes.EasyConfig.Util;
 import org.bukkit.command.*;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  *
@@ -21,17 +26,18 @@ public class LiveEditCommand extends ConfigComponent implements CommandExecutor 
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (label.equalsIgnoreCase(commandLabel)) {
+        if (label.equalsIgnoreCase(commandLabel)
+                && (sender.hasPermission(cmd.getPermission())
+                    || sender instanceof  ConsoleCommandSender)) {
             if (args.length == 0) {
                 //TODO: Implement usage syntax message
                 return true;
             }
             switch(args[0].toLowerCase()) {
                 case "list":
-                    return dispatchList(sender);
+                    return executeList(sender);
                 case "set":
-                    //TODO: Set command
-                    break;
+                    return executeSet(args);
                 case "get":
                     //TODO:
                     break;
@@ -40,8 +46,19 @@ public class LiveEditCommand extends ConfigComponent implements CommandExecutor 
         return false;
     }
 
-    private boolean dispatchList(CommandSender sender) {
+    private boolean executeList(CommandSender sender) {
         sender.sendMessage(Util.buildPathListMessage(getComponentConfig().getPaths()));
+        return true;
+    }
+
+    private boolean executeSet(String[] args) {
+        if (args.length > 3) {
+            String path = args[1];
+            String value = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+
+            getComponentConfig().setValue(path, value);
+            return true;
+        }
         return true;
     }
 }
