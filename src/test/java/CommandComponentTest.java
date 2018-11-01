976251133527dc1f08492a16d448d494ba50d822
darknuju <darknuju@gmail.com>
2018-11-01 16:55:20 -0400

    Some small prep to wirte better tests
    I'm still learning JUnit and need to devote time to writing more proper tests here. (Especially so I dont have to build and run on a server each time I want to test the commands)

---
+++ src/test/java/CommandComponentTest.java
@@ -0,0 +1,15 @@
+import com.wasykes.EasyConfig.Util;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class CommandComponentTest {
+
+    @Ignore
+    @Test
+    public void TestPathList() {
+        //TODO: Add test
+    }
+}
